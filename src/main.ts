7a3a504f4ac760a22e90b1dce072bc37f6b3079f
Jian Yuan <yuanjian@tesla.com>
2022-07-09 13:25:55 +0800

    url branch

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import * as core from '@actions/core';
 import * as github from "@actions/github";
 
+function isValidUrl(urlString: string): boolean {
+  const urlPattern = new RegExp('^(https?:\\/\\/)?' + // validate protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // validate domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))' + // validate OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // validate port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?' + // validate query string
+    '(\\#[-a-z\\d_]*)?$', 'i'); // validate fragment locator
+  return !!urlPattern.test(urlString);
+}
+
 async function run(): Promise<void> {
   const issue = github.context.payload.issue;
   if (!issue) {
@@ -13,7 +23,11 @@ async function run(): Promise<void> {
     core.warning("empty issue body, return");
     return;
   }
-  core.setOutput("text", body);
+  if (isValidUrl(body)) {
+    core.setFailed("empty issue body, return")
+    return;
+  }
+  core.setOutput("url", body);
 }
 
 run()
